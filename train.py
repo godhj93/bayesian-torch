e9388d1a5d3ffa888cdb8fc379c87ce5bb69fa7b
HJ Shin <godhj@unist.ac.kr>
2024-08-13 22:32:27 +0900

    Add: Alpha

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def main(args):
     train_loader, test_loader = get_dataset(args)
 
     date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    log_path = 'runs/{}/{}/{}/bs{}_lr{}_mc{}_temp_{}_ep{}_kd_{}_moped_{}_{}'.format(args.data, args.model, args.type, args.bs, args.lr, args.mc_runs, args.t, args.epochs, args.distill, args.moped, date)
+    log_path = 'runs/{}/{}/{}/bs{}_lr{}_mc{}_temp_{}_ep{}_kd_{}_alpha_{}_moped_{}_{}'.format(args.data, args.model, args.type, args.bs, args.lr, args.mc_runs, args.t, args.epochs, args.distill, args.alpha, args.moped, date)
     writer = SummaryWriter(log_path)
     
     if args.distill:
@@ -24,7 +24,7 @@ def main(args):
         dnn_model.load_state_dict(torch.load(args.weight))
         print(colored(f"Distilling from {args.weight}", 'green'))
         print(colored(f"Test accuracy of DNN: {test_DNN(dnn_model, test_loader)}", 'green'))
-        model = distill(dnn_model, model, steps = 10000, device = device, writer = writer)
+        model = distill(dnn_model, model, steps = 10000, alpha= args.alpha, device = device, writer = writer)
         
     # Optimizer
     optim = torch.optim.Adam(model.parameters(), lr=args.lr)
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     parser.add_argument('--distill', action='store_true', help='Use distillation')
     parser.add_argument('--weight', type=str, help = 'DNN weight path for distillation')
     parser.add_argument('--moped', action='store_true', help='Use MOPED')
+    parser.add_argument('--alpha', type=float, default= 0.0, help = 'Distill Coefficient')
     args = parser.parse_args()
     
     print(colored(args, 'blue'))
