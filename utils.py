f9b5d5072a5b6a96fcf3c38fe8fe613068fbfeb5
HJ Shin <godhj@unist.ac.kr>
2024-08-21 22:08:25 +0900

    Update: Add Vertical flip

--- utils.py
+++ utils.py
@@ -343,6 +343,7 @@ def get_dataset(args):
         transform_train = transforms.Compose([
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
